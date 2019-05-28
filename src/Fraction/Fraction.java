9ba7c776d349110c04472429e08c7e74d5ffca3c
Sébastien Boisgérault <Sebastien.Boisgerault@gmail.com>
2019-05-28 11:21:32 +0200

    Java Fraction example

---
+++ src/Fraction/Fraction.java
@@ -0,0 +1,42 @@
+import java.math.BigInteger;
+
+public class Fraction {
+  public int numerator;
+  public int denominator;
+
+  public Fraction(int numerator, int denominator) {
+    this.numerator = numerator;
+    this.denominator = denominator;
+    this.simplify();
+  }
+
+  public Fraction(int numerator) {
+    this(numerator, 1);
+  }
+
+  protected void simplify() {
+    int gcd;
+    gcd = (BigInteger.valueOf(this.numerator)).gcd(BigInteger.valueOf(this.denominator)).intValue();
+    this.numerator = this.numerator / gcd;
+    this.denominator = this.denominator / gcd;
+    if (this.denominator < 0) {
+      this.numerator = - this.numerator;
+      this.denominator = - this.denominator;
+    }
+  }
+
+  public Fraction add(Fraction other) {
+    int num, den;
+    num = this.numerator * other.denominator + this.denominator * other.numerator;
+    den = this.denominator * other.denominator;
+    return new Fraction(num, den);
+  }
+
+  public String toString() {
+    if (this.denominator == 1) {
+        return (new Integer(this.numerator)).toString();
+    } else {
+      return (new Integer(this.numerator)).toString() + "/" + (new Integer(this.denominator)).toString();
+    }
+  }
+}
