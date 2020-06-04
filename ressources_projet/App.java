3a1be77d2645f39c2a36c93cc32e2d608dd968b8
Cyril JOLY <cyril.joly@mines-paristech.fr>
2020-06-05 00:38:17 +0200

    add ressources_projet

---
+++ ressources_projet/App.java
@@ -0,0 +1,52 @@
+public class App {
+    public static void main(String[] args) {
+        
+        String filename = "sortie.svg"; // Le nom de votre fichier de sortie
+
+      // Étape 1 (exemple pour un rectangle, mais vous pouvez faire autre chose)
+      
+      double x,y,width,height ;
+      x =10 ; y = 3 ; width = 10 ; height = 5 ; // À personnaliser !
+      Rectangle rect = new Rectangle(x,y,width,height) ; 
+      
+      System.out.println(rect) ; 
+      // conseil : redéfinit toString() pour que le rectangle puisse "se présenter" de façon lisible
+
+      String xmlRectangle = rect.toXML();
+      File.write(filename, xmlRectangle; // Permet de sauvegarder votre xml
+      
+      //--------------------------------------------------------
+      // Étape 2 
+      // Ajouter quelques propriétés (ex. : couleurRemplissage, couleurContour)
+
+      Color couleurRemplissage, couleurContour ; // Vous pouvez aussi utiliser ColorAlpha !!!
+
+
+      //---------------------------------------------------------
+      // Étape 3
+      //
+      // Implémenter une autre primitive sur un modèle similaire
+      //
+      //---------------------------------------------------------
+      // Étape 4
+      //
+      // Faire des dessins avec plusieurs primitives 
+      // 
+      // toXML() des précédentes primitives ne peut plus retourner le XML global
+      //
+      // Idée : prévoir une classe SVG capable de conduire une liste de primitives
+      //        son "toXML()" renverra le SVG global en appelant le toXML() des objets à afficher
+      //
+      // --------------------------------------------------------
+      // Étape 5
+      //
+      // Si ce n'est déjà fait, se poser la question de factoriser le code grâce à l'héritage et les interfaces
+      //
+
+
+
+
+    
+
+    }
+  }
